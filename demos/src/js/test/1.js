a8c79b75f9d6b18f12764f823fdc981ef83df4c2
cczw2010 <71752352@qq.com>
2014-05-07 21:16:27 +0800

    资源上传

---
+++ demos/src/js/test/1.js
@@ -0,0 +1,5 @@
+// 这是public下第一个js
+var  test1="1.js";
+function test1(){
+	alert(test1);
+}
---
+++ demos/src/js/test/2.js
@@ -0,0 +1,9 @@
+// 这是public下第二个js
+var  test2="2.js";
+/**
+ * [test1 description]
+ * @type {String}
+ */
+function test2(){
+	alert(test2);
+}
